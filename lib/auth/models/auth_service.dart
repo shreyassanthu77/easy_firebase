fcf5a1ffa1fcea6aa42b9ee2ef2b1bfd40fcd360
shreyas <shreyassanthu77@gmail.com>
2020-11-08 19:41:02 +0530

    added an of method to get the current auth service

--- lib/auth/models/auth_service.dart
+++ lib/auth/models/auth_service.dart
@@ -1,4 +1,6 @@
+import 'package:easy_firebase/auth/auth_wrapper.dart';
 import 'package:easy_firebase/firebase_auth.dart';
+import 'package:flutter/widgets.dart';
 
 abstract class AuthService {
   AuthService();
@@ -6,6 +8,10 @@ abstract class AuthService {
   User get user;
 
   Stream<User> get authChanges;
+
+  static AuthService of(BuildContext context) {
+    return context.findAncestorWidgetOfExactType<AuthWrapper>().authService;
+  }
 
   Future<void> login();
 
